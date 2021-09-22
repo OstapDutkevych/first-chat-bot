20aa4ea02b7d3827c2d43d15b51d0fbbda4035cc
ostap.dutkevych <ostap.dutkevych@etiya.com>
2021-09-22 11:59:13 +0300

    init

---
+++ index.js
@@ -0,0 +1,59 @@
+const {gameOptions, againOptions } = require('./options');
+const Telegram = require('node-telegram-bot-api');
+
+const token = '2009110287:AAHohSAkZZ1AL5ZY6t46FloDYSeWVewCBl0';
+
+const bot = new Telegram(token, {polling:true});
+
+const chats ={};
+
+
+
+const startGame = async (chatId)=> {
+    await bot.sendMessage(chatId, `I'll guess the number from 0 to 9 and you have to guess the number`)
+    let randomNumber = Math.floor(Math.random() * 10)
+    chats[chatId] = randomNumber;
+    await bot.sendMessage(chatId, 'Start guessing', gameOptions)
+}
+
+const start = () => {
+    bot.setMyCommands([
+        {command:'/start', description:'Initial greeting'},
+        {command:'/info', description:'Get information about users'},
+        {command:'/game', description:'Play game and relax'},
+    ])
+
+    bot.on('message', async msg => {
+        const {text, chat} = msg
+
+        if(text === "/start"){
+            await bot.sendSticker(chat.id,'https://tlgrm.ru/_/stickers/ea5/382/ea53826d-c192-376a-b766-e5abc535f1c9/7.webp')
+            return bot.sendMessage(chat.id, `Hello ${chat.first_name} You are Welcome!! ТИ ПІТУХ ЙОБАНИЙ`)
+        }
+        if(text === "/info"){
+            return bot.sendMessage(chat.id, `Your Name: ${chat.first_name} and Surname: ${chat.last_name}`)
+        }
+        if(text === "/game"){
+           return startGame(chat.id)
+        }
+       return bot.sendMessage(chat.id, 'I don not understand you, try again please!!')
+    })
+
+    bot.on('callback_query', async msg => {
+        console.log(msg)
+        console.log(chats)
+        const { data, message:{chat}} = msg;
+
+        if(data === '/again'){
+            return startGame(chat.id);
+        }
+        if(data === chats[chat.id]){
+            return bot.sendMessage(chat.id, `My congratulations, you guessed the number: ${chats[chat.id]}`, againOptions)
+        }else {
+            return bot.sendMessage(chat.id, `Unfortunately you didn't guess, I guessed the number: ${chats[chat.id]}`, againOptions)
+        }
+
+    })
+}
+
+start()
